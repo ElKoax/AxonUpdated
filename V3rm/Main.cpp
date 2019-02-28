76320444e36dbe882016121575bfdff7eedc864f
ElKoax <47944778+ElKoax@users.noreply.github.com>
2019-02-28 20:03:58 +0000

    Add files via upload

---
+++ V3rm/Main.cpp
@@ -0,0 +1,346 @@
+
+#include "globals.h"
+#include "Bridge.h"
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <iterator>
+#include <fstream>
+#include <intrin.h>
+#include <Tlhelp32.h>
+#include <CommCtrl.h>
+#include <Wininet.h>
+#pragma comment(lib, "wininet.lib")
+using namespace std;
+
+DWORD ScriptContext;
+DWORD ScriptContextVFTable = x(0x187CBD0);
+
+//DWORD grabGlobalStateIndex(DWORD ScriptContext, int idx)
+//{
+//	DWORD* context = reinterpret_cast<DWORD*>(ScriptContext);
+//	return context[idx] - (DWORD)&context[idx];
+//}
+
+using Bridge::m_rL;
+using Bridge::m_L;
+
+void PushGlobal(DWORD rL, lua_State* L, const char* s)
+{
+	r_lua_getglobal(rL, s);
+	Bridge::push(rL, L, -1);
+	lua_setglobal(L, s);
+	r_lua_pop(rL, 1);
+}
+
+using namespace std;
+std::string replace_all(std::string subject, const std::string& search, const std::string& replace) {
+	size_t pos = 0;
+	while ((pos = subject.find(search, pos)) != std::string::npos) {
+		subject.replace(pos, search.length(), replace);
+		pos += replace.length();
+	}
+	return subject;
+}
+
+
+static int UserDataGC(lua_State *Thread) {
+	void *UD = lua_touserdata(Thread, 1);
+	if (m_rL) {
+
+		r_lua_rawgeti(m_rL, LUA_REGISTRYINDEX, (int)UD);
+		if (r_lua_type(m_rL, -1) <= R_LUA_TNIL) {
+			lua_pushnil(Thread);
+			lua_rawseti(Thread, LUA_REGISTRYINDEX, (int)UD);
+
+		}
+	}
+	return 0;
+}
+void Executeee(std::string Script) {
+	// script=Instance.new('LocalScript') script.Name = 'KEKE' \r\n"
+	//Script = "spawn(function()" + Script + "\r\nend)";
+	//Script = "script=Instance.new('LocalScript') script.Name = 'Kek' \r\n";
+
+	if (luaL_loadstring(m_L, Script.c_str()))
+		printf("Error: %s\n", lua_tostring(m_L, -1));
+	else
+		lua_pcall(m_L, 0, 0, 0);
+
+	UserDataGC(m_L);
+}
+
+
+namespace Memory {
+	///////////////
+	// ScriptContext Scanner, Pretty Damn Fast
+	// Credits To Whoever Made It (I Believe It Was Louka Or Rakion99)
+	///////////////
+
+	bool compare(BYTE* address, BYTE* pattern, BYTE* mask) {
+		for (; *mask; address++, pattern++, mask++) {
+			if (*mask == 'x' && *address != *pattern) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	DWORD Scan(BYTE* aob, BYTE* mask, BYTE prot = (PAGE_READONLY | PAGE_READWRITE | PAGE_WRITECOPY | PAGE_EXECUTE | PAGE_EXECUTE_READ | PAGE_EXECUTE_READWRITE | PAGE_EXECUTE_WRITECOPY)) {
+		MEMORY_BASIC_INFORMATION mbi;
+		DWORD j = (DWORD)GetModuleHandle(NULL);
+		while (j < 0x7FFFFFFF && VirtualQuery((void*)j, &mbi, sizeof(mbi))) {
+			if (!(mbi.Protect & PAGE_GUARD) && (mbi.State & MEM_COMMIT) && (mbi.Protect & prot)) {
+				for (DWORD k = (DWORD)mbi.BaseAddress; k < ((DWORD)mbi.BaseAddress + mbi.RegionSize); ++k) {
+					if (compare((BYTE*)k, (BYTE*)aob, (BYTE*)mask)) {
+						return k;
+					}
+				}
+			}
+			j += mbi.RegionSize;
+		}
+		return 0;
+	}
+	///////////////
+}
+
+
+DWORD WINAPI input(PVOID lvpParameter)
+{
+	string WholeScript = "";
+	HANDLE hPipe;
+	char buffer[999999];
+	DWORD dwRead;
+	hPipe = CreateNamedPipe(TEXT("\\\\.\\pipe\\Axon"),
+		PIPE_ACCESS_DUPLEX | PIPE_TYPE_BYTE | PIPE_READMODE_BYTE,
+		PIPE_WAIT,
+		1,
+		999999,
+		999999,
+		NMPWAIT_USE_DEFAULT_WAIT,
+		NULL);
+	while (hPipe != INVALID_HANDLE_VALUE)
+	{
+		if (ConnectNamedPipe(hPipe, NULL) != FALSE)
+		{
+			while (ReadFile(hPipe, buffer, sizeof(buffer) - 1, &dwRead, NULL) != FALSE)
+			{
+				buffer[dwRead] = '\0';
+				try {
+					try {
+						WholeScript = WholeScript + buffer;
+					}
+					catch (...) {
+					}
+				}
+				catch (std::exception e) {
+
+				}
+				catch (...) {
+
+				}
+			}
+			/*if (luaL_loadstring(m_L, WholeScript.c_str()))
+				printf("Error: %s\n", lua_tostring(m_L, -1));
+			else
+				lua_pcall(m_L, 0, 0, 0);*/
+			Executeee(WholeScript);
+
+			WholeScript = "";
+		}
+		DisconnectNamedPipe(hPipe);
+	}
+}
+
+
+bool CompareData(const char* Data, const char* Pattern, const char* Mask) {
+	while (*Mask) {
+		if (*Mask != '?') {
+			if (*Data != *Pattern) {
+				return false;
+			};
+		};
+		++Mask;
+		++Data;
+		++Pattern;
+	};
+	return true;
+};
+
+
+DWORD ScanForScriptContext(const char* SCVFT_Offsetted) {
+	MEMORY_BASIC_INFORMATION BasicMemoryInformation = {};
+	SYSTEM_INFO SystemInformation = {};
+	GetSystemInfo(&SystemInformation);
+	DWORD StartingMemorySearchPosition = (DWORD)SystemInformation.lpMinimumApplicationAddress;
+	DWORD MaximumSearchBoundary = (DWORD)SystemInformation.lpMaximumApplicationAddress;
+	do {
+		while (VirtualQuery((void*)StartingMemorySearchPosition, &BasicMemoryInformation, sizeof(BasicMemoryInformation))) {
+			if ((BasicMemoryInformation.Protect & PAGE_READWRITE) && !(BasicMemoryInformation.Protect & PAGE_GUARD)) {
+				for (DWORD Key = (DWORD)(BasicMemoryInformation.BaseAddress); ((Key - (DWORD)(BasicMemoryInformation.BaseAddress) < BasicMemoryInformation.RegionSize)); ++Key) {
+					if (CompareData((const char*)Key, SCVFT_Offsetted, "xxxx")) {
+						return Key;
+					};
+				};
+			};
+			StartingMemorySearchPosition += BasicMemoryInformation.RegionSize;
+		};
+	} while (StartingMemorySearchPosition < MaximumSearchBoundary);
+	return 0x0;
+};
+
+int getRawMetaTable(lua_State *L) {
+	Bridge::push(L, m_rL, 1);
+
+	if (r_lua_getmetatable(m_rL, -1) == 0) {
+		lua_pushnil(L);
+		return 0;
+	}
+	Bridge::push(m_rL, L, -1);
+
+	return 1;
+}
+
+
+
+
+
+void* placeHook(DWORD address, void* hook, bool revert = false) {
+	DWORD oldprot;
+	if (!revert) {
+		void* oldmem = new void*;
+		void* result = new void*;
+		memcpy(oldmem, (void*)address, sizeof(void*) * 4);
+		VirtualProtect((LPVOID)address, 1, PAGE_EXECUTE_READWRITE, &oldprot);
+		*(char*)address = 0xE9; *(DWORD*)(address + 1) = (DWORD)hook - address - 5;
+		memcpy(result, oldmem, sizeof(void*) * 4);
+		VirtualProtect((LPVOID)address, 1, oldprot, &oldprot);
+		return result;
+	}
+	else {
+		VirtualProtect((LPVOID)address, 1, PAGE_EXECUTE_READWRITE, &oldprot);
+		memcpy((void*)address, hook, sizeof(void*) * 4);
+		VirtualProtect((LPVOID)address, 1, oldprot, &oldprot);
+		return NULL;
+	}
+}
+
+DWORD RLSorg = 0;
+DWORD hookaddr = 0x008037C0; //gettop addr
+
+int gettopd(DWORD rState)
+{
+	RLSorg = rState;
+	return (*(DWORD *)(rState + 24) - *(DWORD *)(rState + 12)) >> 4;
+}
+
+void ConsoleBypass(const char* Title) {
+	DWORD aaaa;
+	VirtualProtect((PVOID)&FreeConsole, 1, PAGE_EXECUTE_READWRITE, &aaaa);
+	*(BYTE*)(&FreeConsole) = 0xC3;
+	AllocConsole();
+	SetConsoleTitleA(Title);
+	freopen("CONOUT$", "w", stdout);
+	freopen("CONIN$", "r", stdin);
+	HWND ConsoleHandle = GetConsoleWindow();
+	::SetWindowPos(ConsoleHandle, HWND_TOPMOST, 0, 0, 0, 0, SWP_DRAWFRAME | SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
+	::ShowWindow(ConsoleHandle, SW_NORMAL);
+}
+
+
+void main()
+{
+	ConsoleBypass("Axon ");
+	printf("Hooking To Gettop....\n");
+	void* old = placeHook(x(hookaddr), gettopd); // credits to robo
+	do { Sleep(1); } while (RLSorg == 0); // credits to robo
+	placeHook(x(hookaddr), old, 1); // take it without permission = ill copyright strike your life
+	m_rL = RLSorg;
+	printf("Done Hooking Gettop!\n");
+
+	printf("YEET\n");
+	m_L = luaL_newstate();
+	printf("YEETED\n");
+	Bridge::VehHandlerpush();
+	printf("YEET\n");
+	luaL_openlibs(m_L);
+	luaL_newmetatable(m_L, "garbagecollector");
+	lua_pushcfunction(m_L, UserDataGC);
+	lua_setfield(m_L, -2, "__gc");
+	lua_pushvalue(m_L, -1);
+	lua_setfield(m_L, -2, "__index");
+	PushGlobal(m_rL, m_L, "game");
+	PushGlobal(m_rL, m_L, "Game");
+	PushGlobal(m_rL, m_L, "workspace");
+	PushGlobal(m_rL, m_L, "Workspace");
+	PushGlobal(m_rL, m_L, "Axes");
+	PushGlobal(m_rL, m_L, "BrickColor");
+	PushGlobal(m_rL, m_L, "CFrame");
+	PushGlobal(m_rL, m_L, "Color3");
+	PushGlobal(m_rL, m_L, "ColorSequence");
+	PushGlobal(m_rL, m_L, "ColorSequenceKeypoint");
+	PushGlobal(m_rL, m_L, "NumberRange");
+	PushGlobal(m_rL, m_L, "NumberSequence");
+	PushGlobal(m_rL, m_L, "NumberSequenceKeypoint");
+	PushGlobal(m_rL, m_L, "PhysicalProperties");
+	PushGlobal(m_rL, m_L, "Ray");
+	PushGlobal(m_rL, m_L, "Rect");
+	PushGlobal(m_rL, m_L, "Region3");
+	PushGlobal(m_rL, m_L, "Region3int16");
+	PushGlobal(m_rL, m_L, "TweenInfo");
+	PushGlobal(m_rL, m_L, "UDim");
+	PushGlobal(m_rL, m_L, "UDim2");
+	PushGlobal(m_rL, m_L, "Vector2");
+	PushGlobal(m_rL, m_L, "Vector2int16");
+	PushGlobal(m_rL, m_L, "Vector3");
+	PushGlobal(m_rL, m_L, "Vector3int16");
+	PushGlobal(m_rL, m_L, "Enum");
+	PushGlobal(m_rL, m_L, "Faces");
+	PushGlobal(m_rL, m_L, "Instance");
+	PushGlobal(m_rL, m_L, "math");
+	PushGlobal(m_rL, m_L, "warn");
+	PushGlobal(m_rL, m_L, "typeof");
+	PushGlobal(m_rL, m_L, "type");
+	PushGlobal(m_rL, m_L, "spawn");
+	PushGlobal(m_rL, m_L, "Spawn");
+	PushGlobal(m_rL, m_L, "print");
+	PushGlobal(m_rL, m_L, "printidentity");
+	PushGlobal(m_rL, m_L, "ypcall");
+	PushGlobal(m_rL, m_L, "Wait");
+	PushGlobal(m_rL, m_L, "wait");
+	PushGlobal(m_rL, m_L, "delay");
+	PushGlobal(m_rL, m_L, "Delay");
+	PushGlobal(m_rL, m_L, "tick");
+	PushGlobal(m_rL, m_L, "LoadLibrary");
+	printf("Globals Have been sucessfully wrapped!\n");
+	lua_register(m_L, "getrawmetatable", getRawMetaTable);
+	lua_newtable(m_L);
+	lua_setglobal(m_L, "_G");
+	printf("YEETED ON FUNCTIONS\n");
+	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)input, NULL, NULL, NULL);
+	printf("RVX INJECTED!\n");
+//	luaL_dostring(m_L, "script=Instance.new('LocalScript') script.Name = 'GayAxon' \r\n");
+	MessageBoxA(NULL, "OK!", "Loaded", MB_OK);
+//	MessageBoxA(NULL, "Credits to RoboMat for his hook", MB_OK);
+
+	MessageBoxA(NULL, "Credits to ElKoax, and RoboMat, and ElKoax again for the BANGING addies :DDD ", "Credits", MB_OK);
+}
+
+
+
+BOOL APIENTRY DllMain(HMODULE Module, DWORD Reason, void* Reserved)
+{
+	switch (Reason)
+	{
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(Module);
+		CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)main, NULL, NULL, NULL);
+		break;
+	case DLL_PROCESS_DETACH:
+		break;
+	default: break;
+	}
+
+	return TRUE;
+}
---
+++ V3rm/r_lua.h
@@ -0,0 +1,206 @@
+#pragma once
+#include <Windows.h>
+#include "globals.h"
+#include "memory.h"
+
+#define r_lua_tostring(rL,i)	r_lua_tolstring(rL, (i), NULL)
+#define r_lua_pop(rL,n)		r_lua_settop(rL, -(n)-1)
+#define r_lua_getglobal(rL,s)	r_lua_getfield(rL, LUA_GLOBALSINDEX, (s))
+#define r_lua_newtable(rL) r_lua_createtable(rL, 0, 0)
+
+DWORD unprotect(DWORD addr)
+{
+	BYTE* tAddr = (BYTE*)addr;
+	do
+	{
+		tAddr += 16;
+	} while (!(tAddr[0] == 0x55 && tAddr[1] == 0x8B && tAddr[2] == 0xEC));
+
+	DWORD funcSz = tAddr - (BYTE*)addr;
+
+	PVOID nFunc = VirtualAlloc(NULL, funcSz, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	if (nFunc == NULL)
+		return addr;
+
+	memcpy(nFunc, (void*)addr, funcSz);
+
+	BYTE* pos = (BYTE*)nFunc;
+	BOOL valid = false;
+	do
+	{
+		if (pos[0] == 0x72 && pos[2] == 0xA1 && pos[7] == 0x8B) {
+			*(BYTE*)pos = 0xEB;
+
+			DWORD cByte = (DWORD)nFunc;
+			do
+			{
+				if (*(BYTE*)cByte == 0xE8)
+				{
+					DWORD oFuncPos = addr + (cByte - (DWORD)nFunc);
+					DWORD oFuncAddr = (oFuncPos + *(DWORD*)(oFuncPos + 1)) + 5;
+
+					if (oFuncAddr % 16 == 0)
+					{
+						DWORD relativeAddr = oFuncAddr - cByte - 5;
+						*(DWORD*)(cByte + 1) = relativeAddr;
+
+						cByte += 4;
+					}
+				}
+
+				cByte += 1;
+			} while (cByte - (DWORD)nFunc < funcSz);
+
+			valid = true;
+		}
+		pos += 1;
+	} while ((DWORD)pos < (DWORD)nFunc + funcSz);
+
+	if (!valid)
+	{
+		VirtualFree(nFunc, funcSz, MEM_RELEASE);
+		return addr;
+	}
+
+	return (DWORD)nFunc;
+}
+
+typedef void(__stdcall* rgetfield)(DWORD rL, int idx, const char *k);
+rgetfield r_lua_getfield = (rgetfield)unprotect(x(0x008032D0));
+
+
+typedef char*(__stdcall* rtolstring)(DWORD rL, int idx, size_t *size);
+rtolstring r_lua_tolstring = (rtolstring)(unprotect(x(0x008056B0)));
+
+
+typedef void(__cdecl* rsettop)(DWORD rL, int idx);
+rsettop r_lua_settop = (rsettop)(unprotect(x(0x00805470)));
+
+
+typedef bool(__cdecl* toboolean)(DWORD rL, bool idx);
+toboolean r_lua_toboolean = (toboolean)(x(0x008055F0));
+
+
+typedef void(__fastcall* pushvalue)(DWORD rL, DWORD idx);
+pushvalue r_lua_pushvalue = (pushvalue)(unprotect(x(0x00804730)));
+
+
+typedef double(__thiscall* pushnumber)(DWORD rL, double idx);
+pushnumber r_lua_pushnumber = (pushnumber)(unprotect(x(0x008045E0)));
+
+
+typedef void(__fastcall* rpushstring)(DWORD rL, const char*);
+rpushstring r_lua_pushstring = (rpushstring)(x(0x00804670));
+
+
+typedef int(__cdecl* pcall)(DWORD rL, DWORD, DWORD, DWORD);
+pcall r_luad_pcall = (pcall)(unprotect(x(0x008040B0)));
+
+
+typedef void(__cdecl* pushnil)(DWORD);
+pushnil r_lua_pushnil = (pushnil)(unprotect(x(0x00804570)));
+
+
+typedef DWORD(__cdecl* next2)(DWORD rL, int idx);
+next2 r_lua_next = (next2)(unprotect(x(0x00803F30)));
+
+
+typedef bool(__cdecl* rboolean)(unsigned int, bool);
+rboolean r_lua_pushboolean = (rboolean)(unprotect(x(0x00804170)));
+
+
+typedef double(__cdecl* rtonumber)(DWORD, int);
+rtonumber r_lua_tonumber = (rtonumber)(x(0x008059B0));
+
+
+typedef void(__stdcall* rpushcclosure)(DWORD rL, int fn, int non);
+rpushcclosure r_lua_pushcclosure = (rpushcclosure)(unprotect(x(0x008041F0)));
+
+
+typedef void(__cdecl *rcreatetable)(DWORD rL, int num, int fix);
+rcreatetable r_lua_createtable = (rcreatetable)(unprotect(x(0x00802F80)));
+
+
+typedef void*(__cdecl *rnewuserdata)(DWORD rL, int idx);
+rnewuserdata r_lua_newuserdata = (rnewuserdata)(unprotect(x(0x00803B90)));
+
+
+typedef void*(__stdcall *rgetmetatable)(DWORD rL, int idx);
+rgetmetatable r_lua_getmetatable = (rgetmetatable)(unprotect(x(0x00803590)));
+
+
+typedef void*(__cdecl *rsettable)(DWORD rL, int);
+rsettable r_lua_settable = (rsettable)(unprotect(x(0x008053E0)));
+
+
+typedef DWORD(__cdecl *rtype)(DWORD, int);
+rtype r_lua_type = (rtype)(x(0x00805B10));
+
+
+typedef DWORD(__cdecl *rnewthread)(DWORD);
+rnewthread r_lua_newthread = (rnewthread)unprotect(x(0x00803D80));
+
+
+typedef DWORD(__cdecl *rref)(DWORD, DWORD);
+#include "retcheck.h"
+rref r_luaL_ref = (rref)(Ret::unprotect<DWORD>((byte*)(x(0x007FE1E0))));
+
+
+typedef void(__cdecl *rrawgeti)(DWORD, DWORD, DWORD);
+rrawgeti r_lua_rawgeti = (rrawgeti)unprotect(x(0x008049E0));
+
+
+typedef int(__cdecl *gettop)(DWORD);
+gettop r_lua_gettop = (gettop)(x(0x008037C0));
+
+
+typedef void(__cdecl *rpushlight)(DWORD, void*);
+rpushlight r_lua_pushlightuserdata = (rpushlight)(unprotect(x(0x00804450)));
+
+
+typedef void*(__cdecl *touserdata)(DWORD, int);
+touserdata r_lua_touserdata =(touserdata)Retcheck::Unprotect(x(0x00805AE0), true);
+
+/*
+Sehchainfaker
+Eternals sehchain fixed for windows7, windows 8,...
+*/
+void fakeChain(DWORD* chain)
+{
+	chain[1] = 0x1555555;
+	if ((((DWORD*)chain[0])[1]) != NULL) {
+		((DWORD*)chain[0])[1] = 0x1555555;
+	}
+}
+void restoreChain(DWORD* chain, DWORD unk, DWORD nextUnk)
+{
+	chain[1] = unk;
+	if ((((DWORD*)chain[0])[1]) != NULL) {
+		((DWORD*)chain[0])[1] = nextUnk;
+	}
+}
+
+/*
+r_lua_pcall
+r_lua_pcall we use this to see what error roblox returns if it returns the error we want we put a breakpoint on it
+and rewrite it to our own functions sadly r_lua_pcall has a check sehchaincheck which i already bypassed!
+*/
+
+int r_lua_pcall(DWORD rL, int nargs, int nresults, int errfunc)
+{
+	DWORD* exceptionChain = (DWORD*)__readfsdword(0);
+	DWORD unk = exceptionChain[1];
+	((((DWORD*)exceptionChain[0])[1]) != NULL );
+	{
+		DWORD nextUnk = ((DWORD*)exceptionChain[0])[1];
+		fakeChain(exceptionChain);
+		int ret = r_luad_pcall(rL, nargs, nresults, errfunc);
+		restoreChain(exceptionChain, unk, nextUnk);
+		return ret;
+	}
+
+	fakeChain(exceptionChain);
+	int ret = r_luad_pcall(rL, nargs, nresults, errfunc);
+	restoreChain(exceptionChain, unk, 0);
+	return ret;
+}
